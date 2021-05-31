00a49da7a7d8ee2ff7a2e355dd4ecac9837fe322
mtmiranda <mattheus_miranda@hotmail.com>
2021-05-31 20:36:32 -0300

    creating new Home page component and adding for rendering

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Home from "./Pages/Home/Home";
+import "./Styles/globalStyles.scss";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
