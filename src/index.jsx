148c071e0dd27423c8365a7e3d589c97f3f839d1
mtmiranda <mattheus_miranda@hotmail.com>
2021-06-01 11:26:55 -0300

    updating new index structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './Pages/Home/Home';
 
-import Home from "./Pages/Home/Home";
 import "./Styles/globalStyles.scss";
 
 ReactDOM.render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
+
