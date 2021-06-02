4cd9a9b0ecf397cc1e376c99231207ce3304389e
mtmiranda <mattheus_miranda@hotmail.com>
2021-06-02 16:33:46 -0300

    creating routes for navigation in the app

---
+++ src/Routes/routes.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { Router, Route, Switch } from "react-router";
+
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+import Home from "../Pages/Home/Home";
+import { NotFound } from "../Components/NotFound";
+
+import PrivateRoute from "./privateRoute";
+
+import { history } from "./history";
+
+const Routes = () => {
+  return (
+    <Router history={history}>
+      <Switch>
+        <Route component={Login} exact path="/login" />
+        <Route component={Register} exact path="/register" />
+        <PrivateRoute component={Home} exact path="/" />
+        <PrivateRoute component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
