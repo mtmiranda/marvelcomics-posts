bf4a22c7d12db9f11eb953ef6ab66569bfa258a1
mtmiranda <mattheus_miranda@hotmail.com>
2021-06-04 19:40:14 -0300

    new accordion and informations inside comic card

--- src/Components/Hq/index.jsx
+++ src/Components/Hq/index.jsx
@@ -1,6 +1,19 @@
 import React from "react";
 
+import { AccordionDetails, AccordionSummary } from "@material-ui/core";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import MuiAccordion from "@material-ui/core/Accordion";
+
+import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles.module.scss";
+
+const Accordion = withStyles({
+  root: {
+    fontSize: "small",
+    borderRadius: "5px",
+  },
+  expanded: {},
+})(MuiAccordion);
 
 export const Hq = ({ comic }) => {
   return (
@@ -11,6 +24,33 @@ export const Hq = ({ comic }) => {
           src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
           alt={`${comic.title}`}
         />
+
+        <div className={styles.comicInfo}>
+          <p>${comic.prices[0].price}</p>
+          <p>{comic.pageCount} pages</p>
+        </div>
+
+        <Accordion TransitionProps={{ unmountOnExit: true }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+            <p>Characters</p>
+          </AccordionSummary>
+          <AccordionDetails>
+            {comic.characters.items.map((item) => (
+              <p>
+                {console.log(item)}
+                {item.name.length !== 0 ? (
+                  item.name
+                ) : (
+                  <p>Not found in database</p>
+                )}
+              </p>
+            ))}
+          </AccordionDetails>
+        </Accordion>
       </div>
     </div>
   );
