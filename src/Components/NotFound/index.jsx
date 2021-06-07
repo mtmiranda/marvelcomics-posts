e7186f928317ac6603b61a5e762679aa43efcbdf
mtmiranda <mattheus_miranda@hotmail.com>
2021-06-07 14:08:07 -0300

    styling notFound page

--- src/Components/NotFound/index.jsx
+++ src/Components/NotFound/index.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import styles from "./styles.module.scss";
+import { Footer } from "../../Components/Footer";
+
 export const NotFound = () => {
   return (
-    <div>
+    <div className={styles.notFoundContainer}>
       <h1>Not Found</h1>
+      <Footer />
     </div>
   );
 };
