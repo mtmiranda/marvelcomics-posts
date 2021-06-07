75fbf5ac7ce094bd98d8976a50ad8584920b05fd
mtmiranda <mattheus_miranda@hotmail.com>
2021-06-07 20:28:05 -0300

    returning proxy config

--- src/api/_config/server.js
+++ src/api/_config/server.js
@@ -7,29 +7,11 @@ const cors = require("@koa/cors");
 const app = new Koa();
 const router = new Router();
 
-const allowlist = ["https://marvelcomics-posts.herokuapp.com"];
-
-const corsOptionsDelegate = (req, callback) => {
-  let corsOptions;
-
-  let isDomainAllowed = allowlist.indexOf(req.header("Origin")) !== -1;
-  let isExtensionAllowed = req.path.endsWith(".jpg");
-
-  if (isDomainAllowed && isExtensionAllowed) {
-    // Enable CORS for this request
-    corsOptions = { origin: true };
-  } else {
-    // Disable CORS for this request
-    corsOptions = { origin: false };
-  }
-  callback(null, corsOptions);
-};
-
 module.exports = () => {
   applyRoutes(router);
 
   app
-    .use(cors(corsOptionsDelegate))
+    .use(cors())
     .use(bodyParser())
     .use(router.routes())
     .use(router.allowedMethods());
