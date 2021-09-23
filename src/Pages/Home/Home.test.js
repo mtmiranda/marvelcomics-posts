637fd88adc76db18bfc5a63a5c1d05d50ed38279
Matheus Miranda <49617567+mtmiranda@users.noreply.github.com>
2021-09-23 17:39:39 -0300

    Delete Home.test.js

--- src/Pages/Home/Home.test.js
+++
@@ -1,9 +0,0 @@
-import React from "React";
-import { render, screen } from '@testing-library/react';
-import Home from './Home';
-
-test('renders learn react link', () => {
-  render(<Home />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
